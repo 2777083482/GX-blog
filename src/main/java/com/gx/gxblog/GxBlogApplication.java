f70794eddc31337734e140289dc16433d8eb0c54
gx <2777083482@qq.com>
2024-12-20 23:07:32 +0800

    第一次提交

---
+++ src/main/java/com/gx/gxblog/GxBlogApplication.java
@@ -0,0 +1,13 @@
+package com.gx.gxblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GxBlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GxBlogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gx/gxblog/GxBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gx.gxblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GxBlogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
